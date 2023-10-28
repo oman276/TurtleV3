c92a5f30058ddd8356771c8ece09c59befdd7344
oman276 <75223638+oman276@users.noreply.github.com>
2023-10-28 15:31:54 -0400

    enemy working

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            NewMovement nm = collision.gameObject.GetComponent<NewMovement>();
+            Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();          
+            rb.velocity = Vector2.zero;
+            Vector2 thisPos = this.transform.position;
+            Vector2 newVector = (this.GetComponent<Collider2D>().ClosestPoint(collision.gameObject.transform.position)
+                - thisPos).normalized;
+            rb.AddForce(newVector * nm.speed);
+            print(newVector * nm.speed);
+        }
+        if (collision.tag != "Enemy") {
+            Destroy(this.gameObject);
+        }
+    }
+}
