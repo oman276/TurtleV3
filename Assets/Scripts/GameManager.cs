f843fd2646bdc50e211c1145cd33773de8c6a800
oman276 <75223638+oman276@users.noreply.github.com>
2023-10-29 16:48:29 -0400

    it works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public int level = 1;
     public int enemiesInLevel = 1;
 
+    public float bestTime = 276f;
+
+    Timer timerObj;
+
+    //public TextMeshProUGUI timerText;
+
     public void EnemyDestroyed() {
         enemiesInLevel--;
         if (enemiesInLevel == 0) {
-            StartCoroutine(Restart());
+            //StartCoroutine(Restart());
+            EndGame();
         }
     }
 
@@ -25,7 +34,27 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(levelName);
     }
 
+    void EndGame() {
+        timerObj = FindObjectOfType<Timer>();
+        if ((Time.time - timerObj.startTime) < bestTime) {
+            bestTime = (Time.time - timerObj.startTime);
+        }
+
+        GameObject.Find("Timer").SetActive(false);
+        PlayerHealth ph = FindObjectOfType<PlayerHealth>();
+        ph.canMove = false;
+
+        int minutes = Mathf.FloorToInt(bestTime / 60F);
+        int seconds = Mathf.FloorToInt(bestTime - minutes * 60);
+
+        string niceTime = "Best Time: " + string.Format("{0:0}:{1:00}", minutes, seconds);
+        timerObj.endScreen.SetActive(true);
+        timerObj.bestTimeText.text = niceTime;
+        
+    }
+
     public void LoadLevel1(){
+        enemiesInLevel++;
         SceneManager.LoadScene("test 1");
     }
 }
