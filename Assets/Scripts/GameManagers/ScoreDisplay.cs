93bf210a5cd33621f6e881b03cfcd1e78bc0238e
oman276 <75223638+oman276@users.noreply.github.com>
2024-01-28 17:08:28 -0500

    working high scores

--- Assets/Scripts/GameManagers/ScoreDisplay.cs
+++ Assets/Scripts/GameManagers/ScoreDisplay.cs
@@ -10,10 +10,22 @@ public class ScoreDisplay : MonoBehaviour
 
     private void Start()
     {
-        item = GetComponent<TextMeshProUGUI>();
+        
         ChangeScore();
     }
 
-    public void ChangeScore() { 
+    public void ChangeScore() {
+        item = GetComponent<TextMeshProUGUI>();
+
+        if (GameManager.G.bestTimes.ContainsKey(levelName)) {
+            float bestTime = GameManager.G.bestTimes[levelName];
+            int minutes = Mathf.FloorToInt(bestTime / 60F);
+            int seconds = Mathf.FloorToInt(bestTime - minutes * 60);
+
+            item.text = string.Format("{0:0}:{1:00}", minutes, seconds);
+        }
+        else{
+            item.text = "NEW";
+        }
     }
 }
