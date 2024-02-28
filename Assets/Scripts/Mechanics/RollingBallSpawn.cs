9f33fc15814d8fd2ea5b7394c17d481df34e09de
Owen Gallagher <owengallagher276@gmail.com>
2024-02-27 22:15:49 -0500

    working darts

--- Assets/Scripts/Mechanics/RollingBallSpawn.cs
+++ Assets/Scripts/Mechanics/RollingBallSpawn.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class RollingBallSpawn : MonoBehaviour
 {
-    public float intervalToWait = 4f;
-    public float initDelay = 4f;
+    public float minInterval = 4f;
+    public float maxInterval = 4f;
     public GameObject ball;
     public Transform spawnpoint;
 
+    public float distanceFromPlayer = 15f;
+
+    float timer = 0f;
+    float timeToCall;
+
     private void Start()
     {
-        InvokeRepeating("SpawnBall", initDelay, intervalToWait);
+        timeToCall = Random.Range(minInterval, maxInterval);
     }
-    
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeToCall) {
+            timer = 0f;
+            timeToCall = Random.Range(minInterval, maxInterval);
+            if (Vector2.Distance(this.gameObject.transform.position, GameManager.G.player.transform.position) 
+                <= distanceFromPlayer) {
+                SpawnBall();
+            }
+        }
+    }
+
     void SpawnBall() {
-        GameObject instance = Instantiate(ball, spawnpoint.position, Quaternion.identity);
+        GameObject instance = Instantiate(ball, spawnpoint.position, Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z - 180));
         RigidbodyMove rm = instance.GetComponent<RigidbodyMove>();
         rm.SetDirection(spawnpoint.position - transform.position);
     }
